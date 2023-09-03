a85fb36e72eaa976fa90372a7a7084beeea74ccd
Eszter Barbuscia <gubiscom@yahoo.com>
2023-09-03 10:11:46 -0700

    search books js based on readme

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
@@ -17,8 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("/", (req, res) => {
@@ -29,7 +29,6 @@ app.get("/", (req, res) => {
 const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
-  app.use(routes);
 
   db.once("open", () => {
     app.listen(PORT, () => {
