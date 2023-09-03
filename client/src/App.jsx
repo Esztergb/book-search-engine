7997993e8c2a59642bc39de70c4f09578a1ee3fb
Eszter Barbuscia <gubiscom@yahoo.com>
2023-09-03 12:19:32 -0700

    fixed spelling errors and app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import SearchBooks from "./pages/SearchBooks";
-import SavedBooks from "./pages/SavedBooks";
+// import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import SearchBooks from "./pages/SearchBooks";
+// import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
+import { Outlet } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
@@ -35,16 +36,8 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <>
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={SearchBooks} />
-            <Route exact path="/saved" component={SavedBooks} />
-            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-          </Switch>
-        </>
-      </Router>
+      <Navbar />
+      <Outlet />
     </ApolloProvider>
   );
 }
